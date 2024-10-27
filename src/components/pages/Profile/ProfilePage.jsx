5f01eb7e3e468bc038e3be3e806ac92ca8113781
DerekG29 <derekgrubbs29@gmail.com>
2024-10-27 16:52:48 -0400

    built out profile page and added styling

--- src/components/pages/Profile/ProfilePage.jsx
+++ src/components/pages/Profile/ProfilePage.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
+import '../../../styles/ProfilePage.less';
+
 function ProfilePage() {
   const { user } = useAuth0();
   const { name, picture, email } = user;
 
   return (
-      <div className="profile-container" style={{ height: "300px" }}>
+      <div className="profile-container">
         <div className="profile">
           <img className="profile-picture" src={picture} alt={name} />
-          <div>Username: {name}</div>
-          <div>Email: {email}</div>
+          <div className="user-info">
+            <div className="info-section">
+              <h2>Name</h2>
+              <p>{name}</p>
+            </div>
+            <div className="info-section">
+              <h2>Email</h2>
+              <p>{email}</p>
+            </div> 
+          </div>
         </div>
       </div>
   );
